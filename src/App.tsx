89e006b8927983d5ad4a52bb9e307294bb0a6218
sysnote8main <sysnote8main@gmail.com>
2025-03-12 17:42:54 +0900

    temp commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,26 +8,30 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='grid justify-center'>
+        <div className='bg-blue-50 p-6'>
+          <p>アジ鯖リソースパック</p>
+          <p>リソースパック(テクスチャ)を<span className='underline'>今すぐダウンロード</span>できます。</p>
+        </div>
+        <div className='w-auto'>
+          <p className='bg-amber-100 p-4 flex border-l-2 border-b-2 my-4'>
+            <span className='flex items-center'>
+              <img className='size-20 mr-6' src='https://images.weserv.nl/?url=https://raw.githubusercontent.com/azisaba/resourcepacks/main/life/pack.png&w=128&output=webp' />
+              <div>
+                <div className='text-xl'>Test Server</div>
+                <p className='text-gray-500'>Test!!</p>
+              </div>
+            </span>
+            <span className='flex ml-auto'>
+              <div className='content-end'>
+                <p>a</p>
+                <p>b</p>
+              </div>
+              <div className='content-center'>Download</div>
+            </span>
+          </p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
