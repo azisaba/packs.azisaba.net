5da45643b9c1765daf94ec1b19f145cfab837a60
sysnote8main <sysnote8main@gmail.com>
2025-03-15 21:22:49 +0900

    feat: add site header

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-// import { useState } from 'react'
 import './App.css'
 import { PackCard } from './components/PackCard'
+import SiteHeader from './components/SiteHeader'
 import { PackCardProps } from './types/PackCardProps'
 import { timeAgo } from './util/TimeUtil'
 
 function App() {
+  const exampleProp = {
+    icon: "",
+    name: "conflict",
+    last_updated: timeAgo(new Date("2025-03-13T15:30:17Z")),
+    download_url: "https://github.com/azisaba/resourcepacks/releases/download/action-v9229/conflict.zip",
+    description: "none",
+    supported: "1.10.2"
+  } as PackCardProps
+  
   return (
     <>
+      <SiteHeader />
       <div className='outer'>
-        {/* <h1>Hey!</h1> */}
-        <PackCard props={
-          {
-            icon: "",
-            name: "conflict",
-            last_updated: timeAgo(new Date("2025-03-13T15:30:17Z")),
-            download_url: "https://github.com/azisaba/resourcepacks/releases/download/action-v9229/conflict.zip",
-            description: "none",
-            supported: "1.10.2"
-          } as PackCardProps
-        }/>
+        <PackCard props={exampleProp}/>
       </div>
     </>
   )
---
+++ src/components/SiteHeader.tsx
@@ -0,0 +1,13 @@
+import "./SiteHeader.css"
+
+const SiteHeader: React.FC = () => {
+    return (
+        <div>
+            <h1>アジ鯖リソースパック</h1>
+            <p>リソースパック(テクスチャ)を<u>今すぐダウンロード</u>できます。</p>
+            <p>ダウンロードしたいリソースパックを選択してください。</p>
+        </div>
+    )
+}
+
+export default SiteHeader
