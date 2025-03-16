910bf1ee922c89c762fcf104952abefb1fcddb41
sysnote8main <sysnote8main@gmail.com>
2025-03-16 10:58:47 +0900

    feat: migrate SiteHeader css to tailwind

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -2,8 +2,8 @@ import "./SiteHeader.css"
 
 const SiteHeader: React.FC = () => {
     return (
-        <div className="header">
-            <h1>アジ鯖リソースパック</h1>
+        <div className="header flex flex-col items-center py-14 text-1xl">
+            <h1 className="text-4xl font-bold pb-2">アジ鯖リソースパック</h1>
             <p>リソースパック(テクスチャ)を<u>今すぐダウンロード</u>できます。</p>
             <p>ダウンロードしたいリソースパックを選択してください。</p>
         </div>
