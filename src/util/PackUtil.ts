fbff88119ed4bfd49e6a0f2ed66e2d453bd25316
sysnote8main <sysnote8main@gmail.com>
2025-03-16 12:48:35 +0900

    feat: merge method in PackUtil

--- src/util/PackUtil.ts
+++ src/util/PackUtil.ts
@@ -1,37 +1,44 @@
 import { getReleaseApiUrl } from "../api/GithubApi";
 import { getCachedImageUrl } from "../api/WsrvnlApi";
 import { Release } from "../types/github/GithubResponse";
-import { PackMeta } from "../types/minecraft/PackMeta";
+import { PackMeta } from '../types/minecraft/PackMeta';
 import { PackCardProps } from "../types/PackCardProps";
 import { generalConfig } from "./Config";
 import { timeAgo } from "./TimeUtil";
 
-export async function fetchPacks(): Promise<PackCardProps[]>  {
+export async function fetchPacks(): Promise<PackCardProps[]> {
+    let data: Release
     try {
-        const data: Release = await (await fetch(getReleaseApiUrl())).json();
-        const packraws: PackCardProps[] = data.assets.map(asset => {
-            const name = asset.name.replace(/\.[^/.]+$/, '');
-            return {
-                name: name,
-                icon: getCachedImageUrl(`${generalConfig.ghRawBaseUrl}/${name}/pack.png`),
-                download_url: asset.browser_download_url,
-                last_updated: timeAgo(new Date(asset.updated_at))
-            };
-        });
-
-        const packsData = await Promise.all(
-            packraws.map(async pack => {
-                const mcmeta: PackMeta = await (await fetch(`${generalConfig.ghRawBaseUrl}/${pack.name}/pack.mcmeta`)).json();
-                return {
-                    ...pack,
-                    description: mcmeta.pack.description,
-                    supported: generalConfig.packVersions[mcmeta.pack.pack_format]
-                };
-            })
-        );
-
-        return packsData;
+        data = await (await fetch(getReleaseApiUrl())).json();
     } catch (error) {
-        return Promise.reject(error);
+        return Promise.reject("Failed to fetch github release api: " + error)
     }
+    
+    const packs: PackCardProps[] = await Promise.all(data.assets.map(async asset => {
+        const name = asset.name.replace(/\.[^/.]+$/, '');
+        const target_url = `${generalConfig.ghRawBaseUrl}/${name}`
+
+        const data: PackCardProps = {
+            name: name,
+            icon: getCachedImageUrl(`${target_url}/pack.png`),
+            download_url: asset.browser_download_url,
+            last_updated: timeAgo(new Date(asset.updated_at))
+        }
+
+        // get additional info from pack.mcmeta
+        try {
+            // get mcmeta info
+            const metadata: PackMeta = await (await fetch(`${target_url}/pack.mcmeta`)).json()
+
+            // put additional info
+            data.description = metadata.pack.description
+            data.supported = generalConfig.packVersions[metadata.pack.pack_format]
+        } catch(error) {
+            console.warn("Failed to get mcmeta data", error)
+        }
+
+        return data;
+    }));
+
+    return packs
 }
