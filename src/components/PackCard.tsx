33dd802ed5a1bdd82a9b186d9899f659ec8cdb2d
sysnote8main <sysnote8main@gmail.com>
2025-03-16 12:36:09 +0900

    feat: add alt for pack icon

--- src/components/PackCard.tsx
+++ src/components/PackCard.tsx
@@ -6,8 +6,8 @@ import DownloadIcon from '@mui/icons-material/Download'
 export const PackCard: React.FC<{ props: PackCardProps }> = ({ props }) => {
     return (
         <div className="flex border-l-2 border-b-2 p-5 my-4 justify-between">
-            <div className="head flex">
-                <img className="min-h-32" src={props.icon} />
+            <div className="head flex min-h-32 items-center">
+                <img src={props.icon} alt={`${props.name}のアイコン`}/>
                 <div className="content-center pl-5 min-w-auto">
                     <div className="font-bold">{props.name}</div>
                     <div>{props.description}</div>
